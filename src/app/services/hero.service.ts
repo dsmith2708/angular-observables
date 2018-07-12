ab21b8a2fd628490c031cc80dfbc47e2b2afae2b
Daniel Smith <danielsmith.2796@googlemail.com>
2018-07-12 15:59:02 +0100

    delete function added

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -19,7 +19,6 @@ export class HeroService {
 
   constructor( private http: HttpClient, private messageService: MessageService) { }
 
-
   getHeroes (): Observable<Hero[]> {
     return this.http.get<Hero[]>(this.heroesUrl)
       .pipe(
@@ -46,20 +45,30 @@ export class HeroService {
     );
   }
 
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
+      tap((hero: Hero) => this.log('added hero with id=${hero.id}')),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
+  deleteHero (hero: Hero | number): Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+  
+    return this.http.delete<Hero>(url, httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
+
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
   }
 
   private handleError<T> (operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
-   
-      // TODO: send the error to remote logging infrastructure
-      console.error(error); // log to console instead
-   
-      // TODO: better job of transforming error for user consumption
       this.log(`${operation} failed: ${error.message}`);
-   
-      // Let the app keep running by returning an empty result.
       return of(result as T);
     };
   }
